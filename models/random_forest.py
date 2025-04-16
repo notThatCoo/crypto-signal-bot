0b18da6df7043a4aecd99833f651311341cce27c
Tolu <107754577+notThatCoo@users.noreply.github.com>
2025-04-15 23:19:38 -0700

    Update random_forest.py

--- models/random_forest.py
+++ models/random_forest.py
@@ -5,21 +5,19 @@ def run_model(data):
     data['SMA_5'] = data['close'].rolling(5).mean()
     data['SMA_20'] = data['close'].rolling(20).mean()
     data['Return'] = data['close'].pct_change()
-    data['Volatility'] = data['Return'].rolling(10).std()
-    data['Momentum'] = data['close'] - data['close'].shift(10)
-    data.dropna(inplace=True)
-
     data['Target'] = (data['Return'].shift(-1) > 0).astype(int)
     data.dropna(inplace=True)
 
-    X = data[['SMA_5', 'SMA_20', 'Return', 'Volatility', 'Momentum']]
+    X = data[['SMA_5', 'SMA_20', 'Return']]
     y = data['Target']
-
-    model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=42)
+    model = RandomForestClassifier(n_estimators=100)
     model.fit(X[:-1], y[:-1])
+
+    preds = model.predict(X)
+    data['Prediction'] = preds
 
     latest_features = X.iloc[[-1]]
     prob = model.predict_proba(latest_features)[0][1]
     signal = int(prob > 0.5)
 
-    return signal, prob, data
+    return signal, prob, data, preds
